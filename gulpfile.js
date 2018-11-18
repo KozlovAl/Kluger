8fc9883fe9b006f8c36c1fe7452e25b049781c3a
Alex Kozlov <kau88@rambler.ru>
2018-11-18 13:41:30 +0200

    made two pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var browserSync = require('browser-sync');
+var concat      = require('gulp-concat');
 
 
 
@@ -23,18 +24,33 @@ gulp.task('browser-sync', function() {
 
 // concat js
 
-// gulp.task('scripts', function() {
-//   return gulp.src([
-//         'app/js/jquery-3.3.1.min.js',
-//         'app/js/slick.min.js',
-//         'app/js/main.js'
-//         ])
-//     .pipe(concat('all.js'))
-//     .pipe(gulp.dest('app/js/'));
-// });
+gulp.task('css', function() {
+  return gulp.src([
+        'app/css/libs/slick-theme.css',
+        'app/css/libs/slick.css',
+        'app/css/libs/slick.css',
+        ])
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('app/css/'));
+});
+
+// concat js
+
+gulp.task('scripts', function() {
+  return gulp.src([
+        'app/js/libs/jquery-3.3.1.min.js',
+        'app/js/libs/slick.min.js',
+        'app/js/libs/main.js'
+        ])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('app/js/'));
+});
 
 
-gulp.task('watch', ['browser-sync', 'sass'], function() {
+gulp.task('watch', ['browser-sync', 'css', 'scripts', 'sass'], function() {
+    gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/sass/**/*.scss', ['sass'], browserSync.reload);
+    gulp.watch('app/css/**/*.css', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
    
 });
